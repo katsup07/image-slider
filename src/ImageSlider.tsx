5902dc26f1aa23d39cfd81f1289979680a7dccb6
katsup <brett_7y7@yahoo.ca>
2023-10-05 16:46:22 +0900

    Add buttons for switching to specific picture

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -1,33 +1,64 @@
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
-import { useState } from "react";
+import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
+import { useState } from 'react';
 
 interface Props {
-	images: string[];
-  title: string;
+	images: {url: string; alt: string}[];
+	title: string;
 }
 
 const ImageSlider = ({ images, title }: Props) => {
-  const [imageIndex, setImageIndex] = useState(0);
-  
-  const goForwards = () => imageIndex === images.length - 1 ? setImageIndex(0) : setImageIndex(imageIndex + 1);
-  
-  const goBackwards= () => imageIndex === 0 ? setImageIndex(images.length - 1): setImageIndex(imageIndex - 1);
+	const [imageIndex, setImageIndex] = useState(0);
+
+	const goForwards = () =>
+		imageIndex === images.length - 1
+			? setImageIndex(0)
+			: setImageIndex(imageIndex + 1);
+
+	const goBackwards = () =>
+		imageIndex === 0
+			? setImageIndex(images.length - 1)
+			: setImageIndex(imageIndex - 1);
 
 	return (
-    <>
-      <h1 className='mb-2'>{ title }</h1>
-          <div className='max-w-screen-xl w-96 mb-0 mt-0 mr-auto ml-auto relative'>
-              <img className='w-full object-fit h-72 block' src={images[imageIndex]} onClick={goForwards}/>
-          <div className='flex justify-between'>
-            <button className={`${btnStyles} left-0 bg-transparent image-slider-btn`} onClick={goBackwards}><ArrowBigLeft/></button>
-            <button className={`${btnStyles} right-0 bg-transparent image-slider-btn`} onClick={goForwards}><ArrowBigRight/></button>
+		<section aria-label="image-slider">
+			<h1 className='mb-2'>{title}</h1>
+			<div className='max-w-screen-xl w-96 mb-0 mt-0 mr-auto ml-auto relative'>
+				<div className='flex w-full h-full overflow-hidden '>
+					{images.map(({url, alt}, index) => (
+						<img
+							key={url}
+							className='w-full object-fit h-72 block transition-all duration-500 shrink-0 grow-0'
+							style={{ translate: `${-100 * imageIndex}%` }}
+              aria-hidden={ imageIndex !== index }
+							src={url}
+              alt={alt}
+							onClick={goForwards}
+						/>
+					))}
+				</div>
+				<div className='button-actions flex justify-between'>
+					<button
+						className={`${btnStyles} left-0 bg-transparent image-slider-btn`}
+            aria-label="view previous image"
+						onClick={goBackwards}>
+						<ArrowBigLeft aria-hidden/>
+					</button>
+					<button
+						className={`${btnStyles} right-0 bg-transparent image-slider-btn`}
+            aria-label="view next image"
+						onClick={goForwards}>
+						<ArrowBigRight aria-hidden/>
+					</button>
+          <div className='flex items-center justify-center absolute bottom-4 left-12 translate-y-1/4'>
+            { images.map((_, index) => <button className='bg-transparent text-center text-xs hover:bg-orange-600/30 focus:bg-orange-600/30 rounded-lg' key={index} aria-label={`view image ${index}`} onClick={() => setImageIndex(index)}>{index + 1}</button>)}
           </div>
-          
-          </div>
-    </>
+				</div>
+			</div>
+   
+		</section>
 	);
 };
 
 export default ImageSlider;
 
-const btnStyles = 'block absolute top-0 bottom-0 cursor-pointer'
+const btnStyles = 'block absolute top-0 bottom-0 cursor-pointer';
--- src/assets/halloween-images.ts
+++ src/assets/halloween-images.ts
@@ -7,4 +7,13 @@ import halloween6 from '../images/halloween-6.jpg';
 import halloween7 from '../images/halloween-7.jpg';
 import halloween8 from '../images/halloween-8.jpg';
 
-export const images = [halloween1, halloween2, halloween3, halloween4, halloween5, halloween6, halloween7, halloween8];
+export const images = [
+	{ url: halloween1, alt: 'halloween picture 1' },
+	{ url: halloween2, alt: 'halloween picture 2' },
+	{ url: halloween3, alt: 'halloween picture 3' },
+	{ url: halloween4, alt: 'halloween picture 4'},
+	{ url: halloween5, alt: 'halloween picture 5'},
+	{ url: halloween6, alt: 'halloween picture 6'},
+	{ url: halloween7, alt: 'halloween picture 7'},
+	{ url: halloween8, alt: 'halloween picture 8'},
+];
